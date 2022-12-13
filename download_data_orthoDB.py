5656a6ad8699c947725733cf5730da7ef543435a
Chuan Dong <45725241+RiversDong@users.noreply.github.com>
2022-12-13 12:06:19 +0800

    Update download_data_orthoDB.py
    Allows to get homologous genes of each species without indirect parsing of html. store the result in json

--- download_data_orthoDB.py
+++ download_data_orthoDB.py
@@ -5,27 +5,49 @@ import time
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import os
+import json
 
-genes = []
-f = r'D:\GageTracker\bin\problem_info'
-outpath = r'D:\GageTracker\data\orthoDB_1'
+def get_homo(driver, groupIndex):
+    # --展开第一个group--#
+    species2homo = {}
+    g1_xpath = '//*[@id="group{0}"]/div[6]/div[2]/ul/li[1]/div/ul'.format(groupIndex)
+    g1_content = driver.find_element("xpath",g1_xpath).text
+    g1_content = g1_content.split("\n")
+    g1_content_len = 0
+    for item in g1_content:
+        if "Drosophila" in item:
+            g1_content_len = g1_content_len + 1
+    for item in range(1, g1_content_len+1):
+        g1_js = 'document.querySelector("#group{0} > div:nth-child(6) > div.orthologs > ul > li:nth-child(1) > div > ul > li:nth-child({1}) > span > span.fancytree-expander").click()'.format(groupIndex,item)
+        driver.execute_script(g1_js)
+    g1_gene_xpath = '//*[@id="group{0}"]/div[6]/div[2]/ul/li[1]/div/ul'.format(groupIndex)
+    raw_gene = g1_content = driver.find_element("xpath",g1_gene_xpath).text
+    raw_gene = raw_gene.split("Drosophila ")[1:]
+    for item in raw_gene:
+        item_list = item.split("\n")
+        species = "Drosophila " + item_list[0].split(", ")[0]
+        homogenes = item_list[1:]
+        species2homo[species] = homogenes
+    #print(species2homo)
+    return species2homo
+    # --展开第一个group--#
 
-f = open(f).read().split("\n")
-for i in f:
-    if "groups" in i:
-        gene = i.split(",")[2].split("&")[0].split("=")[1]
-        genes.append(gene)
+age_f = r"D:\GageTracker\result\GageTracker_r1_0.5.age.protein_coding"
+outpath = r'D:\GageTracker\data\orthoDB_2'
 
+age_data = pd.read_csv(age_f, sep="\t")
+genes = list(age_data["Gene"])
 option = webdriver.ChromeOptions()
 option.add_argument('headless')
 driver = webdriver.Chrome(chrome_options=option)
+#genes = ["FBgn0002781", "FBgn0000055", "FBgn0031441"]
 for i in genes:
     i = i.replace(".html", "")
     outpath_f = os.path.join(outpath, i + ".html")
-    outf = open(outpath_f,"w", encoding='utf-8')
+    #outf = open(outpath_f,"w", encoding='utf-8')
     i_request = "https://www.orthodb.org/?query={0}&level=7214&species=7214".format(i)
     driver.get(i_request)
-    
+
     # 计算一下找到几个同源簇
     xpath = '//*[@id="summary"]/div[1]'
     WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, xpath)))
@@ -41,12 +63,13 @@ for i in genes:
         if group_num == 1:
             # 找到一个group
             outpath_f = os.path.join(outpath, i + ".html")
-            xpath_cmd = '//*[@id="group0"]/div[5]/div[2]/ul/li/span/span[2]/span[1]'
-            WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, xpath_cmd)))
-            html = driver.page_source
-            outf.write(html)
-            outf.close()
-            print("OK!", i_request)
+            xpat_wait = '//*[@id="group0"]/div[6]/div[2]/ul/li[1]/span/span[2]/span[1]'
+            WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, xpat_wait)))
+            species2homo = get_homo(driver, 0)
+            OUT = open(os.path.join(outpath, i), "w")
+            json.dump(species2homo,OUT)
+            OUT.close()
+
         else:
             # 找到两个group
             js = 'document.querySelector("#group0 > div > div.s-group-header-arrow.s-group-header-arrow-right > a").click()'
@@ -57,7 +80,13 @@ for i in genes:
             WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, xpat_wait)))
             xpath_wait = '//*[@id="group1"]/div[6]/div[2]/ul/li/span/span[2]/span[1]'
             WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, xpat_wait)))
-            html = driver.page_source
-            outf.write(html)
-    outf.close()
+            species2homo_1 = get_homo(driver, 0) #展开第一个group
             
+            OUT = open(os.path.join(outpath, i+"_g1"), "w")
+            json.dump(species2homo_1,OUT)
+            OUT.close()
+            
+            species2homo_2 = get_homo(driver, 1) #展开第二个group
+            OUT = open(os.path.join(outpath, i+"_g2"), "w")
+            json.dump(species2homo_2,OUT)
+            OUT.close()
